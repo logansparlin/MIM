a6a77ae7bd8e6308be671245b2d56746465ff85e
Logan Sparlin <logan.sparlin@gmail.com>
2015-10-21 15:31:40 -0500

    update

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(express.static(__dirname + '/dist'));
 // });
 
 app.use('/', express.static(__dirname + '/dist'))
+console.log('getting static files')
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+// IP 104.236.101.112
