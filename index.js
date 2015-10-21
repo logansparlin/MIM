807b6a025448d389e83b86c2a5e3d8cb299711f3
Logan Sparlin <logan.sparlin@gmail.com>
2015-10-21 15:26:20 -0500

    updated start script

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 
+
 app.set('port', (process.env.PORT || 4000));
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+
+app.use('/', express.static(__dirname + '/dist'))
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
